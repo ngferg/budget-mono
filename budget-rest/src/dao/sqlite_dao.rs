55298d2ebf006d8818b486dbae7fa206818c054a
ngferg <me@nicholaus-ferguson.us>
2025-11-22 13:04:50 -0600

    insert first budget by default

--- budget-rest/src/dao/sqlite_dao.rs
+++ budget-rest/src/dao/sqlite_dao.rs
@@ -1,5 +1,6 @@
 use crate::dao::Dao;
 use crate::types;
+use chrono::Datelike;
 
 pub(crate) struct SqliteDao {}
 
@@ -17,14 +18,30 @@ impl Dao for SqliteDao {
         std::fs::create_dir_all("dbs")
             .map_err(|e| types::dao::CreateUserError::Internal(e.to_string()))?;
         let sqlite_file_path = format!("dbs/{}.db", email_sha);
-        let sqlite_file = std::fs::OpenOptions::new().write(true)
+        let sqlite_file = std::fs::OpenOptions::new()
+            .write(true)
             .create_new(true)
             .open(sqlite_file_path.clone());
         match sqlite_file {
             Ok(_) => {
-                let conn = rusqlite::Connection::open(sqlite_file_path).expect("Failed to open checked sqlite db");
-                let ddl = std::fs::read_to_string("dbs/USER_DDL.sql").unwrap();
+                let conn = rusqlite::Connection::open(sqlite_file_path)
+                    .expect("Failed to open checked sqlite db");
+
+                let ddl = std::fs::read_to_string("dbs/USER_DDL.sql").expect("DDL sql is missing");
                 conn.execute(ddl.as_str(), ()).unwrap();
+
+                let current_date = chrono::Local::now();
+                let year = current_date.year();
+                let month = current_date.month();
+                conn.execute(
+                    format!(
+                        "INSERT INTO budget (year, month) VALUES ({}, {})",
+                        year, month
+                    )
+                    .as_str(),
+                    (),
+                )
+                .unwrap();
                 Ok(())
             }
             Err(e) => {
