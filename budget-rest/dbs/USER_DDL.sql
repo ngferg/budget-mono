66faa34d79ffa7b91024081988c83881fd6a5aba
ngferg <me@nicholaus-ferguson.us>
2025-11-22 13:31:55 -0600

    categories

--- budget-rest/dbs/USER_DDL.sql
+++ budget-rest/dbs/USER_DDL.sql
@@ -1,5 +1,22 @@
+CREATE TABLE IF NOT EXISTS categories (
+  id INT,  
+  category VARCHAR(31),
+  is_expense BOOLEAN,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO categories
+  (id, category, is_expense)
+VALUES
+  (0, "Income", 0),
+  (1, "House", 1),
+  (2, "Utilities", 1),
+  (3, "Transporttion", 1),
+  (4, "Charity", 1);
+
+
 CREATE TABLE IF NOT EXISTS budget (
-    year int,
-    month int,
+    year INT,
+    month INT,
     PRIMARY KEY (year, month)
 );
