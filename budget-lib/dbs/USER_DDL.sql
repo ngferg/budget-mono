39d7f9f795a3d748193f6d19231337a6f3a927cb
ngferg <me@nicholaus-ferguson.us>
2025-12-02 20:54:45 -0600

    add line_item to ddl

--- budget-lib/dbs/USER_DDL.sql
+++ budget-lib/dbs/USER_DDL.sql
@@ -19,3 +19,22 @@ CREATE TABLE IF NOT EXISTS budget (
     PRIMARY KEY (year, month)
 );
 
+CREATE TABLE IF NOT EXISTS line_item (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  description TEXT,
+  amount INTEGER,
+  category INTEGER,
+  budget_year INTEGER,
+  budget_month INTEGER,
+  FOREIGN KEY(category) REFERENCES categories(id),
+  FOREIGN KEY(budget_year, budget_month) REFERENCES budget(year, month)
+);
+
+INSERT INTO line_item
+  (amount, description, category, budget_year, budget_month)
+VALUES
+  (100000, "Rent", 2, 2025, 12),
+  (15000, "Electric", 1, 2025, 12),
+  (200000, "First paycheck", 0, 2025, 12),
+  (200000, "Second paycheck", 0, 2025, 12);
+
