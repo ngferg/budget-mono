cae1ce2b8bdd1368b5d0cb3daad02e33ca4be185
Nicholaus Ferguson <me@nicholaus-ferguson.us>
2026-02-15 23:13:14 -0600

    Resources

--- budget-web-app/src/App.vue
+++ budget-web-app/src/App.vue
@@ -2,11 +2,26 @@
 import { store } from './store.js'
 import Login from './components/Login.vue'
 import Budget from './components/Budget.vue'
+import Resources from './components/Resources.vue'
 import { ref } from 'vue'
+
+const show_login = ref(!store.is_logged_in);
+const show_budget = ref(store.is_logged_in);
+const show_resources = ref(false);
 
 const handleLogout = () => {
   store.log_out();
   location.reload();
+}
+
+const handleResources = () => {
+  show_budget.value = false;
+  show_resources.value = true;
+}
+
+const handleBudget = () => {
+  show_resources.value = false;
+  show_budget.value = true;
 }
 </script>
 
@@ -14,15 +29,20 @@ const handleLogout = () => {
   <nav class="fixed top-0 left-0 right-0 z-50 w-screen bg-gray-800 py-1 shadow-md m-0">
     <div class="flex justify-between items-center px-8">
       <h1 class="text-white m-0 text-sm"><img src="/fe.png" class="h-16 w-16 mr-2" alt="febudget.com" /></h1>
-      <button v-if="store.is_logged_in" @click="handleLogout"
-        class="bg-red-500 text-white px-4 py-1 rounded cursor-pointer text-sm transition-colors hover:bg-red-600">
-        Log Out
-      </button>
+      <div class="flex gap-4 items-center">
+        <button v-if="show_budget" @click="handleResources">Resources</button>
+        <button v-if="store.is_logged_in && !show_budget" @click="handleBudget">Budget</button>
+        <button v-if="store.is_logged_in" @click="handleLogout"
+          class="bg-red-500 text-white px-4 py-1 rounded cursor-pointer text-sm transition-colors hover:bg-red-600">
+          Log Out
+        </button>
+      </div>
     </div>
   </nav>
   <div class="mt-24">
-    <Login v-if="!store.is_logged_in" />
-    <Budget v-if="store.is_logged_in" />
+    <Login v-if="show_login" />
+    <Budget v-if="show_budget" />
+    <Resources v-if="show_resources" />
   </div>
 </template>
 
---
+++ budget-web-app/src/components/Resources.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Resources</h1>
+        <p>So you finished your budget for the month, and now you want to explore some additional resources to help you
+            manage your finances better. Below are some useful links and tools to guide you on your financial journey,
+            and help you save some money along the way.</p>
+        <ul>
+            <li><a href="https://www.chime.com/r/nicholausferguson/">Chime</a>: Chime is a nice, modern banking app that
+                offers fee-free checking and savings accounts, along with a variety of financial tools to help you
+                manage your money. The savings account also has competitive interest rates. When you open an account
+                with this link, and set up a minimum $200 direct deposit,
+                you will receive a $100 bonus.</li>
+            <li><a href="https://i.capitalone.com/GXIZYUJcE">Capital One QuickSilver</a>: Capital One QuickSilver is a
+                credit card that offers 1.5% cashback rewards on every purchase. On the MasterCard network, and has no
+                annual fee.</li>
+            <li><a
+                    href="https://www.reliant.com/en/private/residential/deeplink/referrees-customer-referral?txtReferralID=LO1HYNL">Reliant
+                    Energy</a>: Live in Texas? Sign up for Reliant Energy using this link to receive a $50 bill credit
+                when you sign up for service.</li>
+        </ul>
+    </div>
+</template>
