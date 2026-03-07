26dc7f277c218f72b5006a270599303f822c18b2
Nicholaus Ferguson <me@nicholaus-ferguson.us>
2026-03-07 16:35:56 -0600

    ads

--- budget-web-app/vite.config.js
+++ budget-web-app/vite.config.js
@@ -1,24 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite';
+import adsense from 'vite-plugin-adsense';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/auth': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/auth/, ''),
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+  return {
+    plugins: [vue(), tailwindcss(), isProduction && adsense()].filter(Boolean),
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+        '/auth': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/auth/, ''),
+        }
       }
     }
-  }
+  };
 })
